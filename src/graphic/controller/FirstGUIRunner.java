9bf13c8656e63e4f679c6d398caf7b0180ac0528
JohnathanFlint <nicholas.white142@gmail.com>
2016-11-01 07:56:47 -0600

    Created the runner and controller.

---
+++ src/graphic/controller/FirstGUIRunner.java
@@ -0,0 +1,10 @@
+package graphic.controller;
+
+public class FirstGUIRunner
+{
+	public static void main (String[] args )
+	{
+		FirstGUIController GUIAppController = new FirstGUIController();
+		GUIAppController.start();
+	}
+}
